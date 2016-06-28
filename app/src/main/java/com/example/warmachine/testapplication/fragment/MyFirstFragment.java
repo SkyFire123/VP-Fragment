94d39a5bc29bfea5c1bb9474b89fe0ffd8944a25
warmachine <q358036146@163.com>
2016-06-28 14:39:58 +0800

    添加SecondFragment布局恩建

--- app/src/main/java/com/example/warmachine/testapplication/fragment/MyFirstFragment.java
+++ app/src/main/java/com/example/warmachine/testapplication/fragment/MyFirstFragment.java
@@ -14,31 +14,31 @@ import com.example.warmachine.testapplication.R;
 /**
  * Created by WarMachine on 2016/6/27.
  */
-public class MyFragment extends Fragment{
+public class MyFirstFragment extends Fragment{
     public static final String ARG_PAGE = "ARG_PAGE";
     public static final String Tag = "MyFramgent";
     private int mPage;
 
-    public static MyFragment newInstance(int page){
+    public static MyFirstFragment newInstance(int page){
         Log.i(Tag,"初始化MyFragment");
         Bundle args = new Bundle();
         args.putInt(ARG_PAGE,page);
-        MyFragment myFragment = new MyFragment();
-        myFragment.setArguments(args);
-        return myFragment;
+        MyFirstFragment myFirstFragment = new MyFirstFragment();
+        myFirstFragment.setArguments(args);
+        return myFirstFragment;
     }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mPage = getArguments().getInt(ARG_PAGE);
-        Log.i(Tag,"MyFragment OnCreate");
+        Log.i(Tag,"MyFirstFragment OnCreate");
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        Log.i(Tag,"MyFragment OnCreateView");
+        Log.i(Tag,"MyFirstFragment OnCreateView");
         View view = inflater.inflate(R.layout.fragment,container,false);
         TextView textView = (TextView) view.findViewById(R.id.mytextview);
         textView.setText("Fragment "+mPage);
